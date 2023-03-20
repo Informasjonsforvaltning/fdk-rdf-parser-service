ce1d7503e60a3cd18b138f5990949ee860c9d62e
terjesyl <108797958+terjesyl@users.noreply.github.com>
2023-03-20 11:47:55 +0100

    chore: remove unused func in testconf

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,10 @@
 """Conftest module."""
 from os import environ as env
-import time
 from typing import Any
 
 from aiohttp.test_utils import TestClient as _TestClient
 from dotenv import load_dotenv
 import pytest
-import requests
-from requests.exceptions import ConnectionError
 
 from fdk_rdf_parser_service import create_app
 
@@ -21,15 +18,3 @@ async def client(aiohttp_client: Any) -> _TestClient:
     """Instantiate server and start it."""
     app = await create_app()
     return await aiohttp_client(app)
-
-
-def is_responsive(url: Any) -> Any:
-    """Return true if response from service is 200."""
-    url = f"{url}/ready"
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            time.sleep(2)  # sleep extra 2 sec
-            return True
-    except ConnectionError:
-        return False
