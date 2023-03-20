8930661e76059180cc108060dc69c0ed79758d1b
NilsOveTen <github.nils.ove@tendenes.com>
2023-03-20 15:28:21 +0100

    fix: install pytest-aiohttp for integration tests

--- noxfile.py
+++ noxfile.py
@@ -116,7 +116,7 @@ def integration_tests(session: Session) -> None:
     """Run the integration test suite."""
     args = session.posargs
     session.install(".")
-    session.install("coverage[toml]", "pytest", "pytest-asyncio")
+    session.install("coverage[toml]", "requests", "pytest", "pytest-aiohttp")
     # -rA shows extra test summary info regardless of test result
     try:
         session.run(
