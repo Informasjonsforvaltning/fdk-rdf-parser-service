6cbfa3265465efd06b46b1fbee0aa0710e83064a
terjesyl <108797958+terjesyl@users.noreply.github.com>
2023-03-24 13:31:33 +0100

    fix: noxfile, ignore missing types for requests and nox

--- noxfile.py
+++ noxfile.py
@@ -6,8 +6,8 @@ import nox
 from nox_poetry import Session, session
 
 python_versions = ["3.11"]
-nox.options.envdir = ".cache"
-# To run consecutive nox sessions faster.
+nox.options.envdir = ".cache"  # To run consecutive nox sessions faster.
+locations = "fdk_rdf_parser_service", "tests", "noxfile.py"
 nox.options.sessions = (
     "lint",
     "mypy",
@@ -35,7 +35,7 @@ def black(session: Session) -> None:
     if os.getenv("CI"):
         print("Skipping black in CI")
         return
-    args = session.posargs or ["."]
+    args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
 
@@ -46,7 +46,7 @@ def isort(session: Session) -> None:
     if os.getenv("CI"):
         print("Skipping black in CI")
         return
-    args = session.posargs or ["."]
+    args = session.posargs or locations
     session.install("isort")
     session.run("isort", *args)
 
@@ -64,7 +64,7 @@ def fixlint(session: Session) -> None:
 @session(python=python_versions[0])
 def lint(session: Session) -> None:
     """Lint using flake8."""
-    args = session.posargs or ["."]
+    args = session.posargs or locations
     session.install(
         "flake8",
         "flake8-annotations",
@@ -80,62 +80,74 @@ def lint(session: Session) -> None:
     session.run("flake8", *args)
 
 
-@session(python=python_versions)
+@session(python=python_versions[0])
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
-    args = session.posargs or ["fdk_rdf_parser_service"]
-    session.install(".")
-    session.install("mypy", "pytest")
+    args = session.posargs or locations
+    session.install(".", "mypy", "pytest")
     session.run("mypy", *args)
     if not session.posargs:
         session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
+
+
+@session(python=python_versions[0])
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    args = session.posargs
+    session.install(
+        ".",
+        "coverage[toml]",
+        "pytest",
+        "pytest-cov",
+        "pytest-docker",
+        "pytest-aiohttp",
+        "requests",
+    )
+    session.run(
+        "pytest",
+        "-rA",
+        "--cov",
+        *args,
+    )
 
 
 @session(python=python_versions)
 def unit_tests(session: Session) -> None:
     """Run the unit test suite."""
     args = session.posargs
-    session.install(".")
-    session.install("coverage[toml]", "pytest")
+    session.install(".", "coverage[toml]", "pytest")
     # -rA shows extra test summary info regardless of test result
-    try:
-        session.run(
-            "pytest",
-            "-m",
-            "unit",
-            "-rA",
-            *args,
-        )
-    finally:
-        if session.interactive:
-            session.notify("coverage")
+    session.run(
+        "pytest",
+        "-m",
+        "unit",
+        "-rA",
+        *args,
+    )
 
 
 @session(python=python_versions)
 def integration_tests(session: Session) -> None:
     """Run the integration test suite."""
     args = session.posargs
-    session.install(".")
     session.install(
+        ".",
         "coverage[toml]",
-        "requests",
         "pytest",
+        "pytest-cov",
         "pytest-aiohttp",
         "pytest-docker",
-        "docker-compose",
+        "requests",
+        "types-requests",
     )
     # -rA shows extra test summary info regardless of test result
-    try:
-        session.run(
-            "pytest",
-            "-m",
-            "integration",
-            "-rA",
-            *args,
-        )
-    finally:
-        if session.interactive:
-            session.notify("coverage")
+    session.run(
+        "pytest",
+        "-m",
+        "integration",
+        "-rA",
+        *args,
+    )
 
 
 # @session(python=python_versions[0])
@@ -155,23 +167,11 @@ def integration_tests(session: Session) -> None:
 
 
 @session(python=python_versions[0])
-def coverage(session: Session) -> None:
-    """Produce the coverage report."""
-    # session.skip()
-    args = session.posargs or ["report"]
-    session.install("coverage[toml]")
-    session.run("coverage", *args)
-
-
-# @session(python=python_versions[0])
-# def codecov(session: Session) -> None:
-#     """Upload coverage data."""
-#     session.skip()
-#     session.install("coverage[toml]", "codecov")
-#     # See pyproject.toml for configuration
-#     # --fail-under=0 to NOT fail in coverage, and upload regardless of coverage percent
-#     session.run("coverage", "xml", "--fail-under=0")
-#     session.run("codecov", *session.posargs)
+def codecov_upload(session: Session) -> None:
+    """Create report and upload coverage data."""
+    session.install("coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
 
 
 @session(python=python_versions[0])
