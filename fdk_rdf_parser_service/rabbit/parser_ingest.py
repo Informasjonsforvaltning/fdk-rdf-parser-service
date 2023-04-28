a874e9399ecd931c58c51ee68b12bbc41f507050
terjesyl <108797958+terjesyl@users.noreply.github.com>
2023-04-28 12:44:52 +0200

    chore: remove unused config var

--- fdk_rdf_parser_service/rabbit/parser_ingest.py
+++ fdk_rdf_parser_service/rabbit/parser_ingest.py
@@ -1,17 +1,11 @@
 """Ingester for messages."""
 import logging
 
-from fdk_rdf_parser_service.config import PARSER
-
 
 def ingest_for_index(index_key: str) -> None:
     """Ingest messages."""
     try:
-        parser_endpoint = PARSER["HOST"]
-        logging.info(
-            f"RabbitMQ ingesting index {index_key!r} "
-            f"into fdk-rdf-parser-service: {parser_endpoint}"
-        )
+        logging.info(f"RabbitMQ ingesting index {index_key!r} ")
 
         # Pass message to parse instantiator
         # parse_index()
